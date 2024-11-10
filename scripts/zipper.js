b11712ce8e1d3997fabcbdd2cbe004abafcfbcfa
dihgg <dihgg@dihgg.com>
2024-11-09 23:31:02 -0300

    chore(system): add zipper functions

---
+++ scripts/zipper.js
@@ -0,0 +1,51 @@
+const fs = require('fs-extra');
+const path = require('path');
+const archiver = require('archiver');
+const micromatch = require('micromatch');
+
+const INCLUDE_PATTERNS = [
+    'public/**'
+];
+
+function shouldInclude(filePath) {
+    return micromatch.isMatch(filePath, INCLUDE_PATTERNS);
+}
+
+function addFilesToArchive(archive, dirPath) {
+    const files = fs.readdirSync(dirPath);
+    files
+        .filter(shouldInclude)
+        .forEach(file => {
+            const filePath = path.join(dirPath, file);
+            const stats = fs.statSync(filePath);
+
+            if (stats.isDirectory()) {
+                archive.directory(filePath, file);
+            } else {
+                archive.file(filePath, { name: file });
+            }
+        });
+}
+
+async function createZip() {
+    const packageJsonPath = path.join(process.cwd(), 'package.json');
+    const { name, version } = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+    const zipFileName = `${name}-${version}.zip`;
+
+    const output = fs.createWriteStream(zipFileName);
+    const archive = archiver('zip', { zlib: { level: 9 } });
+
+    output.on('close', () => {
+        console.log(`${zipFileName} has been created.`);
+    });
+
+    archive.pipe(output);
+
+    addFilesToArchive(archive, process.cwd());
+
+    archive.finalize();
+}
+
+createZip().catch(err => {
+    console.error('Error creating zip file:', err);
+});
