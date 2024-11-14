347e6845e8dbb5399ad968881bed5e5a48b35502
dihgg <dihgg@dihgg.com>
2024-11-14 10:36:25 -0300

    chore(data): adding data fetching example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import { graphql } from "gatsby";
 
 import "@styles/pages/index.css"
 
-const IndexPage: React.FC<PageProps> = () => {
+const IndexPage: React.FC<PageProps> = ({data}) => {
   return (
     <main>
       <h1 className={"test"}>Testing Gatsby!</h1>
+      <ul>
+        {data.allPodcastRssFeedEpisode.nodes.map((node) => {
+          const { item } = node;
+          return (
+            <li key={item.link}>{item.title}</li>
+            )
+          })}
+      </ul>
     </main>
   )
 }
@@ -14,3 +23,21 @@ const IndexPage: React.FC<PageProps> = () => {
 export default IndexPage
 
 export const Head: HeadFC = () => <title>Home Page</title>
+
+export const query = graphql`
+  query PodcastQuery {
+    allPodcastRssFeedEpisode {
+      nodes {
+        item {
+          title
+          link
+          itunes {
+            duration
+            image
+            summary
+          }
+        }
+      }
+    }
+  }
+`;
