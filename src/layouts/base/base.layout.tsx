d82c94e4cb4f818b693bc8c20b741ee9af1fe3d2
dihgg <dihgg@dihgg.com>
2024-11-15 12:29:43 -0300

    feat(layouts): added layouts and components

---
+++ src/layouts/base/base.layout.tsx
@@ -0,0 +1,22 @@
+import React, {FunctionComponent, ReactNode} from "react";
+
+import "./base.layout.css";
+import {Header} from "@components";
+
+/**
+ * Base layout
+ * @description The base layout for the site
+ * @param children Content to render in the layout
+ */
+const BaseLayout: FunctionComponent<{children: ReactNode}> = ({children}) => {
+	return (
+		<>
+		<Header />
+		<main data-testid="main-content">
+			{children}
+		</main>
+		</>
+	)
+}
+
+export {BaseLayout};
