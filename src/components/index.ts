eb49cb0d8ae7b5ef249ff02d68e10111947cc5d5
dihgg <dihgg@dihgg.com>
2024-11-16 01:07:34 -0300

    feat(footer): adding dummy footer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
 export * from './header/header.component';
-export {HeaderProps} from "./header/header.component.types";
-export {MenuItem} from "./header/header.component.types";
+export * from './footer/footer.component';
--- src/layouts/base/base.layout.tsx
+++ src/layouts/base/base.layout.tsx
@@ -1,30 +1,31 @@
 import React, {FunctionComponent, ReactNode} from "react";
 
 import "./base.layout.css";
-import {Header} from "@components";
+import {Footer, Header} from "@components";
 
 /**
  * Base layout
  * @description The base layout for the site
  * @param children Content to render in the layout
  */
-const BaseLayout: FunctionComponent<{children: ReactNode}> = ({children}) => {
+const BaseLayout: FunctionComponent<{ children: ReactNode }> = ({children}) => {
 	return (
-		<>
-		<Header menu={[
-			{
-				title: "Home",
-				link: "/"
-			},
-			{
-				title: "Test",
-				link: "/test"
-			}
-		]} />
-		<main data-testid="main-content">
-			{children}
-		</main>
-		</>
+		<div className="main-grid">
+			<Header menu={[
+				{
+					title: "Home",
+					link: "/"
+				},
+				{
+					title: "Test",
+					link: "/test"
+				}
+			]}/>
+			<main data-testid="main-content">
+				{children}
+			</main>
+			<Footer/>
+		</div>
 	)
 }
 
